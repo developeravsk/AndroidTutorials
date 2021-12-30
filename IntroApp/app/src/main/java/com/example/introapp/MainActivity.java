567c6e79d3d8e41f1d37b92720e7432c7f354e1b
developeravsk <readeravskh@gmail.com>
2021-12-30 10:29:01 +0545

    Introduction to UI and Java objects

---
+++ IntroApp/app/src/main/java/com/example/introapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.introapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    Button add, sub;
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        result=findViewById(R.id.result);
+        add=findViewById(R.id.btn1);
+        sub=findViewById(R.id.btn2);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                result.setText(count+"");
+            }
+        });
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                result.setText(String.valueOf(count));
+                Log.e("Subtract button","pressed");
+            }
+        });
+    }
+}
